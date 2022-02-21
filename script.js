d6c29c94f71e396665790ca4d3e3c42ea9ca3d8d
Tunisia Artope <artope@yahoo.com>
2022-02-20 20:17:16 -0500

    add to html, add to css, add to js

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-// const h2 = document.createElement("h2");
-// h2.textContent = "This content added by JavaScript";
-// document.querySelector("body").appendChild(h2);
+fetch('https://bobsburgers-api.herokuapp.com/characters/')
+    .then(response => response.json())
+    .then(charactersData => {
+        // console.log(charactersData)
+        charactersData.forEach(character =>{
+            buildNavList(character)
+        })
+        buildCharacterInfo(charactersData[0])
+    })
+
+
+const navList = document.getElementById('character-list')
+// console.log(navList)
+function buildNavList(character){
+    const pic = document.createElement('img')
+    pic.src = character.image
+    navList.appendChild(pic)
+    // console.log(pic)
+    pic.addEventListener('click', (e)=>{
+        // console.log('click')
+        buildCharacterInfo(character)
+    })
+}
+
+let oneCharacter;
+const characterImage = document.getElementById('character-image')
+const characterName = document.getElementById('character-name')
+const occupation = document.getElementById('occupation')
+const firstEpisode = document.getElementById('first-episode')
+// console.log(characterImage, characterName, occupation, firstEpisode)
+function buildCharacterInfo(character){
+    oneCharacter = character
+    characterImage.src = character.image
+    characterName.textContent = character.name
+    occupation.textContent = character.occupation
+    firstEpisode.textContent = `First episode: ${character.firstEpisode}`
+}
