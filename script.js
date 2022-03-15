5329eedcff8bbc06b538974b3e6895073054e3d0
Tunisia Artope <artope@yahoo.com>
2022-03-15 13:40:34 -0400

    hooked up seen button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//GET data from API
 fetch('https://bobsburgers-api.herokuapp.com/characters/')
     .then(response => response.json())
     .then(charactersData => {
@@ -6,6 +7,7 @@ fetch('https://bobsburgers-api.herokuapp.com/characters/')
             buildNavList(character)
         })
         buildCharacterInfo(charactersData[0])
+        hookupSeenButton()
     })
 
 
@@ -27,6 +29,7 @@ const characterImage = document.getElementById('character-image')
 const characterName = document.getElementById('character-name')
 const occupation = document.getElementById('occupation')
 const firstEpisode = document.getElementById('first-episode')
+const seen = document.getElementById('seen')
 // console.log(characterImage, characterName, occupation, firstEpisode)
 function buildCharacterInfo(character){
     oneCharacter = character
@@ -34,4 +37,22 @@ function buildCharacterInfo(character){
     characterName.textContent = character.name
     occupation.textContent = character.occupation
     firstEpisode.textContent = `First episode: ${character.firstEpisode}`
+    seen.textContent = seen ? 'Seen' : 'Not seen'
+}
+
+let seenButton = document.getElementById('seen')
+function hookupSeenButton(){
+    seenButton.addEventListener('click', (e)=>{
+        console.log('click')
+        if(oneCharacter.seen === true){
+            oneCharacter.seen = false
+        }else{
+            oneCharacter.seen = true
+        }
+        if(oneCharacter.seen === true){
+            seenButton.textContent = 'Seen'
+        }else{
+            seenButton.textContent = 'Not seen'
+        }
+    })
 }
